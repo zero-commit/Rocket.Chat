1d4226f8c31b1de0ed2b861f3f9a44c4f2d51474
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2023-07-20 20:36:40 -0300

    chore: Added e2e tests for new condition

--- apps/meteor/tests/e2e/fixtures/createAuxContext.ts
+++ apps/meteor/tests/e2e/fixtures/createAuxContext.ts
@@ -2,9 +2,9 @@ import type { Browser, Page } from '@playwright/test';
 
 import type { IUserState } from './userStates';
 
-export const createAuxContext = async (browser: Browser, userState: IUserState, ): Promise<{ page: Page }> => {
+export const createAuxContext = async (browser: Browser, userState: IUserState, route = '/'): Promise<{ page: Page }> => {
 	const page = await browser.newPage({ storageState: userState.state });
-	await page.goto('/');
+	await page.goto(route);
 	await page.locator('.main-content').waitFor();
 
 	return { page };
